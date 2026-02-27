b3c146f1844e8499452d26ab9ba680178d2e1e2a
omaquu <yksirotta@gmail.com>
2026-02-27 08:10:15 +0200

    Implement timeline snapping toggle

--- build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\Common7\Tools\VsDevCmd.bat"
-if %errorlevel% neq 0 exit /b %errorlevel%
-"C:\Users\kuron\.cargo\bin\dx.exe" build --platform web --release
